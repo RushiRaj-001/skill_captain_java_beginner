0892372c44f3b9d483344e665d291c94a29aa85f
RushiRaj-001 <rushirajapure111@gmail.com>
2023-12-22 18:35:18 +0530

    Day 2 Assignment Complete.

---
+++ Day 2/DataTypes.java
@@ -0,0 +1,30 @@
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+// The test cases are generated so that the answer can fit in a 32-bit integer.
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Integer Variable
+        int a = 42;
+        System.out.println("Integer Variable :" + a);
+
+        // Double Variable
+
+        double b = 3.14159;
+        System.out.println("Double Variable :" + b);
+
+        // Character Variable
+        Character c = 'A';
+        System.out.println("Character :" + c);
+
+        // Boolean Variable
+        boolean d = true;
+        System.out.println("Boolean Variable :" + d);
+
+        // String Variable
+        String e = "Hello,SkillCaptain!";
+        System.out.println("String Variable :" + e);
+
+    }
+}
