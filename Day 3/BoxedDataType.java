4d73e99b08604a27850717d9b4a18044e4c5b08c
RushiRaj-001 <rushirajapure111@gmail.com>
2023-12-23 19:31:16 +0530

    Day 3 Assignment Completet.

---
+++ Day 3/BoxedDataType.java
@@ -0,0 +1,15 @@
+
+public class BoxedDataType {
+    public static void main(String[] args) {
+
+        // Predefined integer datatype
+        int age = 25;
+
+        // predefined string datatype
+        String name = "Skill Capatain";
+
+        // output
+        System.out.println("Hello, " + name + "!" + " You are " + age + " years old.");
+
+    }
+}
