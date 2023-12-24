1a81c2d36202f9042e0240dbfde4504bd704576e
RushiRaj-001 <rushirajapure111@gmail.com>
2023-12-24 20:17:32 +0530

    Day 4 Assignment Completed.

--- Day 4/Conditions.java
+++ Day 4/Conditions.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
 
 public class Conditions {
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter Second number : ");
+        int num2 = sc.nextInt();
 
         if (num1 > num2) {
-            System.out.println("The larger number is : " + num1);
+            System.out.println("The larger number is : " + num1); // if num1 is larger then print this line
         } else if (num1 < num2) {
-            System.out.println("The larger number is : " + num2);
+            System.out.println("The larger number is : " + num2); // if num2 is larger then this line print
         } else {
-            System.out.println("The numbers are equal");
+            System.out.println("The numbers are equal"); // else both are equal
         }
+
+        sc.close();
     }
 }
