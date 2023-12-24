b55179893bf0803da8fbfd62dcf6b95fa9bd5766
RushiRaj-001 <rushirajapure111@gmail.com>
2023-12-24 19:58:10 +0530

    Day 4 Assignment completed

---
+++ Day 4/Conditions.java
@@ -0,0 +1,16 @@
+
+public class Conditions {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        if (num1 > num2) {
+            System.out.println("The larger number is : " + num1);
+        } else if (num1 < num2) {
+            System.out.println("The larger number is : " + num2);
+        } else {
+            System.out.println("The numbers are equal");
+        }
+    }
+}
