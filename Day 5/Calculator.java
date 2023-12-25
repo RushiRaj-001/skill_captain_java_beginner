ec8a4e6fa5cb1b7d36e733984f279d293cb3afde
RushiRaj-001 <rushirajapure111@gmail.com>
2023-12-25 18:40:54 +0530

    Day 5 Assignment Complete.

---
+++ Day 5/Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the First Number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter The Second Number : ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter the Opaerator (+, - , * , /)");
+        char opaerator = sc.next().charAt(0);
+
+        int Answer;
+        switch (opaerator) {
+
+            case '+': // if Enter Addition operator this case execute
+                Answer = num1 + num2;
+                System.out.println("The Answer is : " + Answer); // output
+                break;
+
+            case '-': // if Enter substraction operator this case execute
+                Answer = num1 - num2;
+                System.out.println("The Answer is : " + Answer); // output
+                break;
+
+            case '*': // if Enter Multiplication operator this case execute
+                Answer = num1 * num2;
+                System.out.println("The Answer is : " + Answer); // output
+                break;
+
+            case '/':// if Enter Division operator this case execute
+                Answer = num1 / num2;
+                System.out.println("The Answer is : " + Answer); // output
+                break;
+
+            default: // if Invalid Operator enter then this default case execute
+                System.out.println("Invalid Operator, Try Again."); // output
+                break;
+        }
+        sc.close();
+    }
+}
